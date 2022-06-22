97b26d9ea54752c36935b25637020825a4211a90
fajartriadyp <triadyputra3@gmail.com>
2022-06-22 22:47:39 +0700

    Project Web

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Yummie Japanese Food || Home</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Menu Section Start-->
+        <div class="menu">
+            <div class="wrapper">
+                tested
+            </div>
+        </div>
+        <!-- Menu Section End-->
+
+        <!-- Main Concent Section Start -->
+        <div class="main-content">
+            <div class="wrapper">
+                test
+            </div>           
+
+        </div>
+        <!-- Main Concent Section End -->
+
+        <!-- Footer Section Start-->
+        <div class="footer">
+            <div class="wrapper">
+                testss
+            </div>
+        </div>
+        <!-- Footer Section End -->
+    </body>
+
+
+</html>
